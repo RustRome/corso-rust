e07b96e451cd27f0d0f68d85bddefcf751587c66
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2019-11-16 20:02:14 +0100

    Variables Declaration

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // simple variable declaration
+    let a = 5;
+
+    // simple str declaration
+    let s = "Rust Evangelism Strike Force";
+
+    // str declaration with explicit type
+    let sr: &str = "Rust Evangelism Strike Force";
+
+    // String declaration
+    let string = String::from("Rust Evangelism Strike Force");
+
+    // variable declaration with type suffix 1
+    let sfx_1 = 32i32;
+
+    // variable declaration with type suffix 2
+    let sfx_2 = 32_i32;
+
+    // variable declaration for humans
+    let very_big = 1_000_000_000;
+}
